929a266fa004dabda342fd0ccd745a4ab2b0f832
kkkkks <64839061+kkkkks@users.noreply.github.com>
2021-03-12 15:10:22 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-
 exports.min = function min (array) {
-  return 0;
+  if(array&&array.length!=0 ){
+return Math.min.apply(0, array)
+  }
+  else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array&&array.length!=0){
+    return Math.max.apply(0, array)
+      }
+      else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  
+ let avgValue = 0;
+ let sum = 0;
+
+ if(array&&array.length>0){
+  for(let i = 0; i <array.length; i++)
+  {
+ sum+=array[i];
+  }
+  return avgValue = sum/array.length;
+ }
+ else return 0;
 }
